05c897a364986bcd8a04d4c2ea380478b997d0d4
Bikotoru Ryzen <you@example.com>
2023-05-19 15:59:07 -0400

    init

---
+++ Demo/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Demo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+        Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+        Routing.RegisterRoute(nameof(SecondPage), typeof(SecondPage));
+
+    }
+}
---
+++ Demo/Platforms/Android/CustomActivity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+
+namespace Demo.Platforms.Android
+{
+
+    [IntentFilter(new[] { Intent.ActionView },
+            Categories = new[]
+            {
+                Intent.ActionView,
+                Intent.CategoryDefault,
+                Intent.CategoryBrowsable,
+
+            },
+            DataHost = "result",
+            DataScheme = "demoapp"
+            //DataPath = "result", 
+            //DataPathPrefix = "/result"
+        )
+    ]
+    [Activity(Exported = true, Theme = "@style/Maui.SplashTheme")]
+    internal class CallbackActivity : MauiAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+
+            base.OnCreate(savedInstanceState);
+
+
+
+            OnNewIntent(Intent);
+
+
+        }
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+
+            var action = intent.Action;
+            var data = intent.DataString;
+
+     
+
+        }
+    }
+
+}
